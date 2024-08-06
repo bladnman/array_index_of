8037c9c9652e09e01ffccd01ddfcfd3c810bb0f6
Matt Maher <matt.maher@sony.com>
2024-08-06 08:43:17 -0700

    move commit

--- src/hackUtils.js
+++ src/hackUtils.js
@@ -4,9 +4,13 @@ const getArray = (length, gap, seed = "42") => {
   const seededRnd = seedrandom(seed);
   const rnd = (min, max) => ~~(seededRnd() * max) + min;
   let last = 0;
-  return [...Array(length)].map((e) => {
-    last = rnd(last, last + gap);
-    return last;
-  });
+  return [...Array(length)]
+    .map((e) => {
+      last = rnd(last, last + gap);
+      return last;
+    })
+    .sort(function (a, b) {
+      return a - b;
+    });
 };
 exports.getArray = getArray;
